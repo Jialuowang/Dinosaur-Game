bbd223c67f7a8d7140b07ae1c9f3dd93ad8e30d7
Sophie.Ciulla <Sophie.Ciulla@MU-C1-09.mullen.pvt.k12.co.us>
2017-10-02 08:59:06 -0600

    changed runner

--- Dinosaur Game/src/Runner.java
+++ Dinosaur Game/src/Runner.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class Runner
 	{
 		static int num;
-		static int dinoChoice;
+		static int userDinoChoice;
+		static int opponentDino;
 
 		public static void main(String[] args)
 			{
@@ -38,27 +39,55 @@ public class Runner
 				
 				
 			}
+		private static void printList()
+		{
+			for (Dinosaur d: DinosaurData.dinoArray)
+				{
+			System.out.println(d.getType());
+				}
+		}
 
 		private static void selectDinosaur()
 			{
 				Scanner userInput = new Scanner(System.in);
 				System.out.println("What dinosaur would you like to be?");
-				for (Dinosaur d: DinosaurData.dinoArray)
-					{
-				System.out.println(d.getType());
-				dinoChoice = userInput.nextInt();
-					}
-				
+				printList();
+				userDinoChoice = userInput.nextInt();
+						
+							System.out.println("You have chosen the " + DinosaurData.dinoArray.get(userDinoChoice-1).getType());
+							System.out.println("	It has an attack damage of " + DinosaurData.dinoArray.get(userDinoChoice-1).getAttackDamage() +  "\n	A defense ability of  " + DinosaurData.dinoArray.get(userDinoChoice-1).getDefenseAbilities() + "\n	A speed of " + DinosaurData.dinoArray.get(userDinoChoice-1).getSpeed() + "\n	And attacks with a " + DinosaurData.dinoArray.get(userDinoChoice-1).getAttackType());
+						
+							DinosaurData.dinoArray.remove(userDinoChoice-1);
 			}
+				
 		private static void selectDinosaurToBattle()
 			{
-				// TODO Auto-generated method stub
-				
+				Scanner userInput = new Scanner(System.in);
+				System.out.println();
+				System.out.println("What dinosaur would you like to battle?");
+				printList();
+				opponentDino = userInput.nextInt();
 			}
 
 		private static void battleOpponent()
 			{
-				// TODO Auto-generated method stub
+				Scanner userInput = new Scanner(System.in);
+				System.out.println("Would you like to run or fight? Press 1 for run and 2 for fight. ");
+				int battleChoice = userInput.nextInt();
+				if (battleChoice == 1)
+					{
+						System.out.println("You have chosen to run");
+						System.out.println();
+						if (DinosaurData.dinoArray.get(opponentDino-1).getSpeed() > DinosaurData.dinoArray.get(userDinoChoice-1).getSpeed())
+						{
+							System.out.println("You tested your luck blindly, and your opponent was faster than you");
+							System.out.println("You will now have to fight.");
+						}
+						else
+							{
+								System.out.println("Wooo close one! \nYou got lucky this time. \nYou outran your opponent, but who knows what will happen next time");
+							}
+					}
 				
 			}
 
